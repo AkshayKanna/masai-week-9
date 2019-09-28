5ff52e6328711322bf19c30e891a0a8adc3d68d7
AKshayKanna <akshaykanna120298@gmail.com>
2019-09-28 13:10:32 +0530

    W9 P

--- src/Components/SearchByGenre.js
+++ src/Components/SearchByGenre.js
@@ -1,6 +1,7 @@
 import React from "react";
-// import styles from "./SearchByGenre.module.css"
+import styles from "./SearchByGenre.module.css"
 import axios from "axios"
+import { thisExpression } from "@babel/types";
 
 class SearchByGenre extends React.Component
 {
@@ -11,8 +12,17 @@ class SearchByGenre extends React.Component
             genre:[],
             input_value:"",
             bring:false,
-            raw : []
+            raw : [],
+            select : "",
+            visible:20,
         }
+        this.loadMore=this.loadMore.bind(this)
+    }
+
+    loadMore(){
+        this.setState((prev) =>{
+            return{visible:prev.visible+20};
+        });
     }
 
     handleInput=(e)=>{
@@ -28,84 +38,33 @@ class SearchByGenre extends React.Component
     		url: 'https://raw.githubusercontent.com/masai-school/assignments-data/master/data/dummy/json/movies_collections.json',
 		})
 		.then((response) =>{
-            console.log(response.data)
+            // console.log(response.data)
 			this.setState({
                  genre:response.data
-                // genre_details:response.data.filter((element)=>{
-                //     return element.genre === this.state.detail;
-                // }).map((element)=>{
-                //     return element
-                // })
-			});	
-        // })
-        // .then((response) =>{
-        //     console.log(response.data)
-        //     let genre_details=response.data.filter((item)=> {
-        //         return item.genre === this.state.input_value;
-        //     }).map((item)=>{
-        //         return item
-        //     })
-        //     console.log(genre_details)
-            // this.setState({t
-                // genre_details:response.data.filter((item)=> {
-                //     return item.genre === "Action";
-                // }).map((item)=>{
-                //     return item
-                // })
-            // });           
+                //  .filter((genre)=>{
+                //      return genre.genre === this.state.select;
+                //  }).map((genre) =>{
+                //      return genre;
+                //  })
+			});	          
         })
 		.catch((err) => alert(err))
     }
 
-   
-
-    // handleInput= e => 
-    //     this.setState({
-    //     [e.target.name]:e.target.value
-    // })
-
     onSubmit =()  =>{
         this.setState({
-            bring:true
+            bring:true     
         })
     }
 
     render()
     {
         console.log(this.state.input_value)
-        // console.log(this.state.genre_details)
-        // var gg=[]
-        // if(this.state.bring)
-        // {
-        //     let input = this.state.input_value
-        
-        //     var json=this.state.genre
-            
-        //     for (var i=0;i < json.length; i++) 
-        //     {
-        //         if(input=== json[i].genre)
-        //         {
-        //             gg.push(json[i])	
-        //         }		
-        //     }
-        //     this.setState({
-        //         bring : false,
-        //         raw : gg
-        //     })
-        // }
-
-        // if(this.state.bring)
-        // {
-        //     this.setState({
-        //         bring : false,
-        //     })
-        // }
-
-        console.log(this.state.raw) 
+        console.log(this.state.genre) 
         
         return(
             <div>
-                <div style={{ background: "#673AB7"}} className="jumbotron jumbotron-fluid ">
+                <div className="jumbotron jumbotron-fluid " style={{background: "linear-gradient(to left, #30cfd0 0%, #330867 100%"}}>
                     <div className="container">
                         <h1 className="display-4">Open Movie Database Using React And Bootstrap</h1>
                     </div>
@@ -119,19 +78,41 @@ class SearchByGenre extends React.Component
                     <button type="button" class="btn btn-primary mb-2" onClick={this.onSubmit}>Search</button>
                 </form>
 
+                {/* <form className={styles.align}>
+                    <div className="form-row align-items-center">
+                        <div className="col-auto my-1">
+                            <label className="mr-sm-2 sr-only " for="inlineFormCustomSelect">Preference</label>
+                                <select onClick ={(e) => {
+                                    this.setState({
+                                        select:e.target.value
+                                    })
+                                }} className="custom-select mr-sm-2" id="inlineFormCustomSelect">
+                                    <option selected>Choose...</option>
+                                    <option value="Comedy">Comedy</option>
+                                    <option value="Drama">Drama</option>
+                                    <option value="Documentary">Documentary</option>
+                                    {/* <option value="box_office_desc">High to Low box_office</option> */}
+                                {/* </select> */}
+                        {/* </div>
+                        <div className="col-auto my-1">
+                            <button type="button" className="btn btn-primary" onClick ={this.onSubmit}>Search</button>
+                        </div>
+                    </div> */}
+                {/* </form> */} */}
+
                 {this.state.bring  ?(
                     <div className="container">
-                        <div class = "row " >
-                            { this.state.genre.map((genre) => {
-                                if(genre.genre===this.state.input_value)
+                        <div className = "row " >
+                            { this.state.genre.slice(0,this.state.visible).map((genre) => {
+                                if(genre.genre === this.state.input_value)  
                                 {
                                     return (
-                                    <div class = "card col-lg-3 col-md-4 col-sm-3 mt-4 ml-2" key = {genre.id}>
-                                        <div class="card-body">
-                                            <h3 class="card-title text-dark" > {genre.title}</h3><br />
-                                            <h5 class="card-subtitle "> GENRE: {genre.genre}</h5>
-                                            <h5 class="card-subtitle "> YEAR: {genre.year}</h5>
-                                            <h5 class="card-subtitle "> BOX_OFFICE: {genre.box_office}</h5>
+                                    <div className = "card col-lg-3 col-md-4 col-sm-3 mt-3 " key = {genre.id} style={{background:"linear-gradient(to right, #0c3483 0%, #a2b6df 100%, #6b8cce 100%, #a2b6df 100%)",color:"white"}}>
+                                        <div class="card-body" className={styles.font} >
+                                            <h3 className="card-title" style={{color:"white"}}>{genre.title}</h3><br /> <hr></hr>
+                                            <h5 className="card-subtitle "> GENRE: {genre.genre}</h5> <hr></hr>
+                                            <h5 className="card-subtitle "> YEAR: {genre.year}</h5> <hr></hr>
+                                            <h5 className="card-subtitle "> BOX_OFFICE: {genre.box_office}</h5>
                                             <br />
                                         </div>
                                     </div>
@@ -139,8 +120,48 @@ class SearchByGenre extends React.Component
                                 }
                                 })
                             }
+                            
+
+
                         </div>
+                        <div className="text-center"> */}
+                            {this.state.visible < this.state.genre.length &&
+                                <button onClick={this.loadMore} className="btn btn-primary mt-3" type="button">Load More</button>
+                            } 
+                        </div>
+
+                        
                     </div>
+                    // <div className="container-fluid">
+                    //     <div className = "row " >
+                    //         { this.state.genre.map((genre) => {
+                    //             if(genre.genre === this.state.select)
+                    //             {
+                    //                 return (
+                    //                 <div className = "card col-lg-3 col-md-4 col-sm-3" key = {genre.id} style={{background: "linear-gradient(45deg, #874da2 0%, #c43a30 100%)",minWidth:"1px"}}>
+                    //                     <div class="card-body" className={styles.font}>
+                    //                         <h3 class="card-title text-dark" > <b>TITLE:{genre.title}</b></h3><br /> <hr></hr>
+                    //                         <h5 class="card-subtitle "> GENRE: {genre.genre}</h5> <hr></hr>
+                    //                         <h5 class="card-subtitle "> YEAR: {genre.year}</h5> <hr></hr>
+                    //                         <h5 class="card-subtitle "> BOX_OFFICE: {genre.box_office}</h5> <hr></hr>
+                    //                         <br />
+                    //                     </div>
+                    //                 </div>
+                    //                 );
+                    //             }
+                    //             })
+                    //         }
+                    //     </div>
+                        /* <div className="text-center">
+                            {this.state.visible < this.state.genre.length && 
+                                <button onClick={this.loadMore} className="btn btn-primary mt-4 text-center" type="button">Load More</button>
+                            }
+                        </div> */
+                       
+                     
+                        
+                    // </div>
+
                     ) : null}
             </div>
         );
--- src/Components/Sort.js
+++ src/Components/Sort.js
@@ -1,7 +1,7 @@
 import React from "react";
-// import styles from "./SearchByGenre.module.css"
+import styles from "./SearchByGenre.module.css"
 import axios from "axios"
-import { thisExpression } from "@babel/types";
+
 
 class Sort extends React.Component
 {
@@ -12,63 +12,128 @@ class Sort extends React.Component
             year:"",
             box_office:"",
             input_value:"",
-            sort_details:[]
+            sort_details:[],
+            bring:false,
+            select : "",
+            visible:20
+
         }
+        this.loadMore=this.loadMore.bind(this)
     }
+
+    loadMore(){
+        this.setState((prev) =>{
+            return{visible:prev.visible +20};
+        });
+    }
+
 
     handleInput=(e)=>{
         this.setState({
-            input_value:e.target.value
+            input_value:e.target.value,
+            bring:false
         })
     }
     
-    get_sort_details=()=>{
+    onSubmit =()=>{
         axios({
             method:"get",
             url: 'https://raw.githubusercontent.com/masai-school/assignments-data/master/data/dummy/json/movies_collections.json',
         })
         .then((response) =>{
             let sort = ""
-            if(this.state.input_val === "year_asc" ){
+            if(this.state.select === "year_asc" ){
                 sort = response.data.sort((val_a, val_b) => val_a.year - val_b.year);
+                this.setState({
+                    sort_details : sort
+                })
+                console.log(sort)
             }
-            if(this.state.input_val === "year_desc" ){
+            
+            if(this.state.select === "year_desc" ){
                 sort = response.data.sort((val_a, val_b) => val_b.year - val_a.year);
+                this.setState({
+                    sort_details : sort
+                })
             }
-            if(this.state.input_val === "box_office_asc" ){
+            if(this.state.select === "box_office_asc" ){
                 sort = response.data.sort((val_a, val_b) => val_a.box_office - val_b.box_office);
+                this.setState({
+                    sort_details : sort
+                })
             }
-            if(this.state.input_val === "box_office_desc" ){
-                sort = response.data.sort((val_a, val_b) => val_a.box_office - val_a.box_office);
+            if(this.state.select === "box_office_desc" ){
+                sort = response.data.sort((val_a, val_b) => val_b.box_office - val_a.box_office);
+                this.setState({
+                    sort_details : sort
+                })
             }
-            this.setState({
-                sort_details : sort
-            })
+            
    })
    .catch((err) => alert(err))
-
+        this.setState({
+            bring:true
+        })
     }
+
 
     render()
     {
         return(
-            <form>
-                <div className="form-row align-items-center">
-                    <div className="col-auto my-1">
-                        <label className="mr-sm-2 sr-only" for="inlineFormCustomSelect">Preference</label>
-                            <select onChange ={this.handleInput} className="custom-select mr-sm-2" id="inlineFormCustomSelect">
-                                <option selected>Choose...</option>
-                                <option value="year_asc">Year Ascending</option>
-                                <option value="year_desc">Year Descending</option>
-                                <option value="box_office_asc">Box Office</option>
-                                <option value="box_office_desc">Box Office</option>
-                            </select>
+            <div>
+                <form className={styles.align}>
+                    <div className="form-row align-items-center">
+                        <div className="col-auto my-1">
+                            <label className="mr-sm-2 sr-only " for="inlineFormCustomSelect">Preference</label>
+                                <select onClick ={(e) => {
+                                    this.setState({
+                                        select:e.target.value
+                                    })
+                                }} className="custom-select mr-sm-2" id="inlineFormCustomSelect">
+                                    <option selected>Choose...</option>
+                                    <option value="year_asc">Low to High Year</option>
+                                    <option value="year_desc">High to Low Year</option>
+                                    <option value="box_office_asc">Low to High box_office</option>
+                                    <option value="box_office_desc">High to Low box_office</option>
+                                </select>
+                        </div>
+                        <div className="col-auto my-1">
+                            <button type="button" className="btn btn-primary" onClick ={this.onSubmit}>Search</button>
+                        </div>
                     </div>
-                    <div className="col-auto my-1">
-                        <button type="button" className="btn btn-primary" onClick ={this.get_sort_details}>Search</button>
+                </form>
+                {this.state.bring  ?(
+                    <div className="container-fluid">
+                        <div className = "row " >
+                            { this.state.sort_details.slice(0,this.state.visible).map((sort_details) => {
+                                if(sort_details.year===this.state.input_value || (this.state.select==="year_asc") || (this.state.select==="year_desc") || (this.state.select==="box_office_asc") ||(this.state.select==="box_office_desc"))
+                                {
+                                    return (
+                                    <div className = "card col-lg-3 col-md-4 col-sm-3" key = {sort_details.id} style={{background: "linear-gradient(45deg, #874da2 0%, #c43a30 100%)",color:"white"}}>
+                                        <div class="card-body" className={styles.font}>
+                                            <h3 class="card-title" > <b>TITLE:{sort_details.title}</b></h3><br /> <hr></hr>
+                                            <h5 class="card-subtitle "> GENRE: {sort_details.genre}</h5> <hr></hr>
+                                            <h5 class="card-subtitle "> YEAR: {sort_details.year}</h5> <hr></hr>
+                                            <h5 class="card-subtitle "> BOX_OFFICE: {sort_details.box_office}</h5> <hr></hr>
+                                            <br />
+                                        </div>
+                                    </div>
+                                    );
+                                }
+                                })
+                            }
+                        </div>
+                        <div className="text-center">
+                            {this.state.visible < this.state.sort_details.length && 
+                                <button onClick={this.loadMore} className="btn btn-primary mt-4 text-center" type="button">Load More</button>
+                            }
+                        </div>
+                       
+                     
+                        
                     </div>
-                </div>
-            </form>
+                    ) : null}
+            </div>    
         );
     }
 }
